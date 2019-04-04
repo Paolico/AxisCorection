f599fcd0eca30b178ce7a1b04f539619dde0b846
Pavel Kadlečík <pavel.kadlecik@centrum.cz>
2019-04-05 00:31:51 +0200

    Výstupní soubor - formátování pro iTNC530

--- src/main/java/sample/SettingAxisCorrectionController.java
+++ src/main/java/sample/SettingAxisCorrectionController.java
@@ -122,6 +122,8 @@ public class SettingAxisCorrectionController implements Initializable {
         stepCompValue = Math.abs(((endCompValue - startCompValue )/ ( targetCount - 1)));
         textFieldStepCompValue.setText(String.valueOf(stepCompValue));
 
+
+
     }
 
 
@@ -161,6 +163,7 @@ public class SettingAxisCorrectionController implements Initializable {
         // todo variable position
 
         axisList = axisListConfigDatabase.get(axisDefs);
+        int axisIndex = 0 ;
 
         StringBuilder sb = new StringBuilder();
        // outFileTextArea.appendText("BEGIN AXIS-X.COM  DATUM:-875.0000 DISTANCE:50.0000");
@@ -176,26 +179,38 @@ public class SettingAxisCorrectionController implements Initializable {
 //        outFileTextArea.appendText(String.format("%1$-5s%2$-12s%3$s", "NR", "", "1=F()"));
 
         if (controlSystem == Constants.iTNC530) {
-            sb.append(String.format("%1$-5s%2$-13s", "NR", ""));
+
+            // TODO Zjistit mezeru mezi sloupci, jestli má být 10 nebo 11 znaků pro iTNC530
+
+            sb.append(String.format("%1$-8s%2$-12s", "NR", ""));
 
             for (AxisDef axis:axisList) {
-                sb.append(String.format("%1$-11s",axis.getLabel()));
+                sb.append(String.format("%1$-10s",axis.getLabel()));
+
+                // zjisteni indexu osy, kterou chci kompenzovat
+                if (axis.getName() ==  axisComp) {
+                   axisIndex = Integer.valueOf(axis.getIndex()) - 1;
+                }
+
             }
 
         }
         else if (controlSystem == Constants.TNC640){
-            sb.append(String.format("%1$-5s%2$-12s%3$s-12s%3$s", "NR", "","AXISPOS", axisComp));
+            sb.append(String.format("%1$-8s%2$-12s%3$s-12s%3$s", "NR", "","AXISPOS", axisComp));
         }
 //        outFileTextArea.appendText(System.getProperty("line.separator"));
         sb.append(System.getProperty("line.separator"));
 
         for (int i = 0; i < meanValue.getBothMean().size(); i++) {
-            Double t = rtlWrap.getRtlTargetData().getTargets().get(i);
+            Double t =  rtlWrap.getRtlTargetData().getTargets().get(i);
             String tPrefix = t >= 0 ? "+" : "";
             Double m = meanValue.getBothMean().get(i);
             String mPrefix = m >= 0 ? "+" : "";
 //            outFileTextArea.appendText(String.format("%1$-5s%2$s%3$-12.4f%4$s%5$.4f", i, tPrefix, t, mPrefix, m / 1000));
-            String row = String.format("%1$-5s%2$s%3$-12.4f%4$s%5$.4f", i, tPrefix, t, mPrefix, m / 1000);
+            double columnWidth =  10;
+            double columnPrefix =  11.4 + columnWidth*axisIndex;
+
+            String row = String.format("%1$-6s%2$3s%3$-"+columnPrefix+"f%4$s%5$.4f", i, tPrefix, t, mPrefix, m / 1000);
             sb.append(row.replace(",", ".")); // String.valueOf(t).replace(",", ".")
 //            outFileTextArea.appendText(System.getProperty("line.separator"));
             sb.append(System.getProperty("line.separator"));
